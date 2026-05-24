b8e8dd641b3aa2d48c6679150a16050cf1f57127
Sourabh Bhalse <sourabhbhalse197@gmail.com>
2026-05-25 04:23:04 +0530

    fixed login

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -168,7 +168,7 @@ const Login = () => {
                         <p>Admin: admin@eventify.com</p>
                         <p>Password: pass123</p>
                         
-                        <p>user can {" "}
+                        <p>user : {" "}
                             <Link to="/register" className="text-blue-600 font-bold">
                                 Register now
                             </Link>
